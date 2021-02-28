85412889f974cce420ff3da528f8cb83f0111029
Herasil <78405491+Herasil@users.noreply.github.com>
2021-02-28 21:48:10 +0200

    Movement/model complete

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    //Vector2 mousePos;
+    public Animator animator;
+    //public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        //Vector2 lookDir = mousePos - rb.position;
+       //float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
+        //rb.rotation = angle;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
