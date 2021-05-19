e980a80b3197208b5e504d049713ca11edfe0cb7
FuryRav3n <78351471+FuryRav3n@users.noreply.github.com>
2021-05-19 19:17:01 +0300

    after gynimas

---
+++ Assets/PostProcs/acs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class acs : MonoBehaviour
+{
+    public PostProcessVolume vol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //vol.profile.TryGetSettings<Bloom>(out var bloom);
+        //bloom.intensity.Override(50f);
+        //vol.profile.TryGetSettings<Vignette>(out var vignette);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeControl t = GameObject.FindWithTag("timebar").GetComponent<TimeControl>();
+        float timeT = t.timeRemaining;
+        float timeMax = t.timerMax;
+        timeT -= Time.deltaTime;
+        vol.profile.TryGetSettings<Vignette>(out var vignette);
+        vignette.intensity.Override(timeT / timeMax);
+
+    }
+}
--- Assets/Scripts/EnemyKill.cs
+++ Assets/Scripts/EnemyKill.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
+
 
 public class EnemyKill : MonoBehaviour
 {
     public Animator death;
     public AudioSource deathSound;
     public AudioSource tookDmg;
+    public PostProcessVolume vol;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Bullet"))
@@ -24,6 +27,8 @@ public class EnemyKill : MonoBehaviour
         {
             TimeControl t = GameObject.FindWithTag("timebar").GetComponent<TimeControl>();
             t.timeRemaining = t.timeRemaining - 5f;
+            vol.profile.TryGetSettings<Bloom>(out var bloom);
+            bloom.intensity.Override(50f);
             tookDmg.Play();
             this.GetComponent<Pathfinding.AIPath>().enabled = false;
             death.Play("Base Layer.Dead");
